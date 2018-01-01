6f29b82eb04b85ab9715df373c4dddec2124f045
Laxmi Garde <laxmi@MacBook-Pro.local>
2018-01-02 00:56:31 +0530

    New

---
+++ target/spring-angularjs-tutorial-1.0-SNAPSHOT/resources/static/js/controller.js
@@ -0,0 +1,12 @@
+app.controller('galleryController', function($scope) {
+    $scope.headingTitle = "Photo Gallery Items";
+});
+
+app.controller('contactusController', function($scope) {
+    $scope.headingTitle = "Contact Info";
+});
+
+app.controller('fccramController',function($scope){
+    $scope.headingTitle = "FCCRAM resources";
+
+});
